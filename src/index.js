5fd3b25947642e26e1b3b44fe3594827c94f6736
Craig bauer <craigbauer23@gmail.com>
2020-08-30 14:26:54 -0400

    Init project files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // change to node-sass - Sass
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
