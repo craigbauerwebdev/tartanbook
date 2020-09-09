6ac65d6cecc97e41a6ec4a48a0dea6763cf6b4bb
Craig bauer <craigbauer23@gmail.com>
2020-09-08 21:36:17 -0400

    consumed data and started building main component and filter ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './Login';
 import TartanBook from './TartanBook';
 
 
-function App() {
-  return (
-    <div className="App">
-      {/* <AuthProvider> */}
-        <Router>
-          <p>Header</p>
-          <Switch>
-            <Route exact path="/">
-              <Login />
-            </Route>
-            <Route path="/home">
-              <TartanBook />
-            </Route>
-          </Switch>
-          <p>Footer</p>
-        </Router>
-      {/* </AuthProvider> */}
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      vendors: null
+    };
+    //this.mobileMenu = this.mobileMenu.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://cors-anywhere.herokuapp.com/http://tartanweddings.com/wp-json/wp/v2/tartanbook", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ vendors: json })
+      })
+      .catch(e => {
+        console.log("error: ", e);
+      });
+  }
+
+  render() {
+
+    if(this.state.vendors) {
+      return (
+        <div className="App">
+          {/* <AuthProvider> */}
+            <Router>
+              <header>
+                <h1>Tartan Book</h1>
+              </header>
+              <Switch>
+                <Route exact path="/">
+                  <Login />
+                </Route>
+                <Route path="/home">
+                  <TartanBook data={this.state.vendors} />
+                </Route>
+              </Switch>
+              <p>Footer</p>
+            </Router>
+          {/* </AuthProvider> */}
+        </div>
+      );
+    } else {
+      return 'loading...';
+    }
+  }
 }
 
 export default App;
--- src/TartanBook.js
+++ src/TartanBook.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 //import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+class TartanBook extends Component {
 
-function TartanBook() {
-  return (
-    <div className="trtanbook">
-      <h1>List of Vendors</h1>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      vendor_type: null,
+      location: null
+    };
+    //this.mobileMenu = this.mobileMenu.bind(this);
+  }
+
+  getVendors = (vendors) => {
+    
+    return (
+      vendors.map((vendor) => {
+        const imgStyle = {
+          backgroundImage: "url("+vendor.fimg_url+")",
+          backgroundSize: "cover"
+        }
+        const markup = <div class="vendor">
+          <div className="image-container" style={imgStyle}>
+            {/* <img src={vendor.fimg_url} /> */}
+          </div>
+          <h2>{vendor.title.rendered}</h2>
+          <strong>{vendor.vendor_type}</strong>
+          <p>{vendor.description}</p>
+        </div>;
+        return markup;
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div className="tartanbook">
+        <div class="center">
+        </div>
+        <div class="filters">
+          <div className="row">
+            <div className="col">
+              <select onChange={this.updateFilter} className="form-control">
+                <option value="All Vendors">All Vendors</option>
+                <option value="Photographers">Photographers</option>
+                <option value="Bands">Bands</option>
+              </select>
+            </div>
+            <div className="col">
+              <select onChange={this.updateFilter} className="form-control">
+                <option value="All Locations">All Locations</option>
+                <option value="Edinburgh">Edinburgh</option>
+                <option value="Glasgow">Glasgow</option>
+              </select>
+            </div>
+          </div>
+        </div>     
+        {this.getVendors(this.props.data)}
+      </div>
+    );
+  }
 }
 
 export default TartanBook;
