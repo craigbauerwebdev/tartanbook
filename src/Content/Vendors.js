44dc49a88edca0bb847f130c7028e5c0a920f868
Craig bauer <craigbauer23@gmail.com>
2020-11-16 09:41:27 -0500

    developed vendor card

--- src/Content/Vendors.js
+++ src/Content/Vendors.js
@@ -21,30 +21,44 @@ class TartanBook extends Component {
     return (
       randomVendors.map((vendor) => {
         const imgStyle = {
-          backgroundImage: "url("+vendor.fimg_url+")",
+          backgroundImage: vendor.fimg_url ? `url("${vendor.fimg_url}")` 
+          : "url(http://tartanweddings.com/wp-content/uploads/2018/11/dundas-events-page.jpg)",
           backgroundSize: "cover",
           backgroundPosition: "top"
         }
         const markup = <div key={vendor.id} className="vendor clearfix">
           <div className="image-container" style={imgStyle}></div>
           <div className="meta">
-            <h2>{vendor.title.rendered}id: {vendor.id}</h2>
+            <h2>{vendor.title.rendered}{/* vendor.id */}</h2>
             <strong>{vendor.vendor_type}</strong>
             <p><i>{vendor.location}</i></p>
             <p>{vendor.description}</p>
           </div>
           <div className="meta links">
-            <img alt="instagram logo" src={process.env.PUBLIC_URL + "/images/instagram.png"} width="35" />
-            <img alt="website logo" src={process.env.PUBLIC_URL + "/images/website.png"} width="35" />
+            {vendor.website &&
+              <a href={vendor.website} target="_blank" rel="noopener noreferrer">
+                <img alt="website logo" src={process.env.PUBLIC_URL + "/images/website.png"} height="35" />
+              </a>
+            }
+            {vendor.instagram &&
+              <a href={vendor.instagram} target="_blank">
+                <img alt="instagram logo" src={process.env.PUBLIC_URL + "/images/instagram.png"} height="35" />
+              </a>
+            }
+            {vendor.facebook &&
+              <a href={vendor.facebook} target="_blank" rel="noopener noreferrer">
+                <img alt="facebook logo" src={process.env.PUBLIC_URL + "/images/facebook.png"} height="35" />
+              </a>
+            }
           </div>
-          <div className="like-btn">
+          {/* <div className="like-btn">
             {vendor.liked &&
               <i className="large material-icons">favorite</i>
             }
             {!vendor.liked &&
               <i className="large material-icons">favorite_border</i>
             }
-          </div>
+          </div> */}
         </div>;
         //console.log(markup);
         return markup;
@@ -54,11 +68,8 @@ class TartanBook extends Component {
 
   render() {
     const { sortBy, vendorType, location, vendorFilters, locationFilters, data } = this.props;
-    /* const TBStyle = {
-      color: "#fff"
-    } */
-    return (
-      
+
+    return (  
       <div className="tartanbook vendors-page">
         <div className="center">
           <Filters vendorType={vendorType} location={location} sortBy={sortBy} vendorFilters={vendorFilters} locationFilters={locationFilters} />
